b2fda8744e4c48d73b323478ac3fff3355850d2b
Devon <dmmartens91@gmail.com>
2023-12-06 15:31:37 +0100

    testing/compling working

--- automate_gpt/src/apis/call_request.rs
+++ automate_gpt/src/apis/call_request.rs
@@ -1,23 +1,33 @@
-use crate::models::general::llm::{Message, ChatCompletion};
+#![allow(unused_imports)]
+use crate::models::general::llm::{Message, ChatCompletion, APIResponse};
 use dotenv::dotenv;
-use reqwest::{Client, header::{self, InvalidHeaderValue}, Request, RequestBuilder, Response, Error};
 use std::env;
+use reqwest::{Client, header::{self, InvalidHeaderValue}, Request, RequestBuilder, Response, Error};
+
 
 // Box for dynamic sized error
 // ownership of type implementing send can be called between threads
-pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Boxd<dyn::std::error:Error + Send>> {
+pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Box<dyn::std::error::Error + Send>> {
     dotenv().ok();
     let api_key = env::var("OPEN_AI_KEY").expect("OPEN_AI_KEY not found");
     let api_org = env::var("OPEN_AI_ORG").expect("OPEN_AI_ORG not found");
     let url = "https://api.openai.com/v1/engines/davinci/completions";
 
     let mut headers = header::HeaderMap::new();
-    headers.insert(header::AUTHORIZATION, header::HeaderValue::from_str(&format!("Bearer {}", api_key))
-        .map_err(e: InvalidHeaderValue -> Box<)
-    );
-    headers.insert("OpenAI-Organization", header::HeaderValue::from_str(&api_org.as_str()).unwrap());
 
-    let client = Client::new();
+    headers.insert(header::AUTHORIZATION, header::HeaderValue::from_str(&format!("Bearer {}", api_key))
+        .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send>)?
+    );  
+    
+    headers.insert("OpenAI-Organization", header::HeaderValue::from_str(&api_org.as_str())
+        .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send>)?
+    );
+
+
+    let client: Client = Client::builder()
+        .default_headers(headers.clone())
+        .build()
+        .map_err(|err| Box::new(err) as Box<dyn std::error::Error + Send>)?; // ? is used to return error if any
 
     let chat_completion = ChatCompletion {
         model: "gpt-4".to_string(),
@@ -28,16 +38,21 @@ pub async fn call_gpt(messages: Vec<Message>) -> Result<String, Boxd<dyn::std::e
     // Constructing the request using RequestBuilder
     let request: RequestBuilder = client
         .post(url)
-        .headers(headers)
+        .headers(headers.clone())
         .json(&chat_completion);
 
     // Sending the request and getting the response
-    let res_raw: Response = request
-        .send()
-        .await
-        .unwrap();
+    let response = request
+    .send()
+    .await
+    .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send>)?;
 
-    dbg!(res_raw.text().await.unwrap());
+    let api_response: APIResponse = response
+        .json::<APIResponse>()
+        .await
+        .map_err(|e| Box::new(e) as Box<dyn std::error::Error + Send>)?;
+
+    Ok(api_response.choices[0].message.content.clone())
 }
 
 #[cfg(test)]
@@ -57,6 +72,14 @@ mod tests {
 
         // Assuming call_gpt function does not return anything.
         // If it returns a Result, you should handle it here.
-        call_gpt(messages).await;
+        let res = match call_gpt(messages).await {
+            Ok(response) => response,
+            Err(e) => {
+                // Handle error, e.g., log it or return early
+                eprintln!("Error calling GPT: {}", e);
+                return;
+            }
+        };
+        
     }
 }
--- automate_gpt/src/models/general/llm.rs
+++ automate_gpt/src/models/general/llm.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize};
+use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Serialize )]
 
@@ -15,4 +15,23 @@ pub struct ChatCompletion {
     pub model: String,
     pub messages: Vec<Message>,
     pub temperature: f32,
+}
+
+#[derive(Deserialize, Debug )]
+
+pub struct APIMessage {
+    pub content: String,
+}
+
+
+#[derive(Deserialize, Debug )]
+
+pub struct APIChoice {
+    pub message: APIMessage,
+}
+
+
+#[derive(Deserialize, Debug )]
+pub struct APIResponse {
+    pub choices: Vec<APIChoice>,
 }
