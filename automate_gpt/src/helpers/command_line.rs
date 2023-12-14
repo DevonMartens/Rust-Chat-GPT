00a2ecbdbbf4bdc764567eb336c38d9ae9516cec
Devon <dmmartens91@gmail.com>
2023-12-14 16:03:36 +0100

    saves progress in helpers

--- automate_gpt/src/helpers/command_line.rs
+++ automate_gpt/src/helpers/command_line.rs
@@ -4,6 +4,35 @@ use crossterm::{
 }; // used to change the color in commnad line for better user experience
 use std::io::{stdin, stdout}; // used to get user input
 
+pub enum PrintCommand {
+    AICall,
+    UnitTest,
+    Issue
+}
+
+impl PrintCommand {
+    pub fn print_agent_message(&self, agent_pos: &str, agent_statment: &str) {
+        let mut stdout = stdout();
+
+        let statement_color = match self {
+            PrintCommand::AICall => Color::Cyan,
+            PrintCommand::UnitTest => Color::Yellow,
+            PrintCommand::Issue => Color::Red,
+        };
+        // print agent position
+        stdout.execute(SetForegroundColor(Color::Green)).unwrap();
+        print!("{}: ", agent_pos);
+
+        // print agent statement
+        stdout.execute(SetForegroundColor(statement_color)).unwrap();
+        println!("{}", agent_statment);
+
+        // reset color
+        stdout
+            .execute(ResetColor)
+            .unwrap();
+    }
+
 // get user request
 pub fn get_user_response(question: &str) -> String {
     let mut stdout = stdout();
--- automate_gpt/src/helpers/general.rs
+++ automate_gpt/src/helpers/general.rs
@@ -1,16 +1,33 @@
-
+use crate::models::general::llm::Message;
+use crate::helpers::command_line::PrintCommand;
 
 pub fn extend_ai_function(ai_func: fn(&str) -> &'static str) -> fn(&str) -> &'static str, funct_input &str) -> Message {
     let ai_func_str = ai_func(funct_input);
-    // Message {
-    //     message: message,
-    //     message_type: MessageType::Text,
-    // }
+    let msg: String = format!("function {}  Instruction: funct printer. With input {}", ai_func_str, funct_input);
+    // return msg;
+    Message {
+        role: "system".to_string(),
+        content: msg,
+    }
+}
+
+pub fn ai_task_request(
+    msg_context: String,
+    agent_pos: &str,
+    agent_operation: &str,
+    function_pass: fn(&str) -> &'static str,
+) -> Srting {
+    let func_msg: Message = extend_ai_function(ai_func: function_pass, funct_input&msg_context);
+
+    PrintCommand::AICall.print_agent_message(agent_pos, agent_operation);
+
+    String::from("Hello World")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::ai_functions::aifunc_managing::convert_user_input_to_goal;
 
     #[test]
     fn test_extend_ai_function() {
@@ -20,4 +37,8 @@ mod tests {
         let ai_func_extended = extend_ai_function(ai_func, "Hello World");
         assert_eq!(ai_func_extended, "Hello World");
     }
+
+    let x_str = convert_user_input_to_goal(
+        //_user_request: 
+        "Hello World");
 }
